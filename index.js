e69f753982a7b5e850b0e08da3168b6b0648ef80
Julie Rindone <julie.rindone@gmail.com>
2025-12-01 11:16:47 -0800

    Created files, wrote html & css

---
+++ index.js
@@ -0,0 +1,3 @@
+const ideaContainer = document.getElementById('idea-container')
+
+ideaContainer.innerText = `Take an advanced course in CSS so you can make the internet just a little more awesome.`
