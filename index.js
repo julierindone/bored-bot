6f393e8afc7efd82fe73418b47949cff87f66bc8
Julie Rindone <julie.rindone@gmail.com>
2025-12-02 17:07:38 -0800

    Added functionality and styles

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const ideaContainer = document.getElementById('idea-container')
+/* When the button is clicked, call out to the Bored API
+(URL: https://apis.scrimba.com/bored/api/activity)
+and replace the h4 with the activity from the API */
 
-ideaContainer.innerText = `Take an advanced course in CSS so you can make the internet just a little more awesome.`
+const activityName = document.getElementById('activity-name')
+const getIdeaButton = document.getElementById('get-idea-btn')
+const headerSection = document.getElementById('header-section')
+
+getIdeaButton.addEventListener('click', () => {
+	fetch("https://apis.scrimba.com/bored/api/activity")
+		.then(response => response.json())
+		.then(data => {
+			console.log(data)
+			activityName.textContent = data.activity
+			activityName.classList.add('transition')
+			document.getElementById('main-section').style.marginTop = '.75em'
+			document.getElementById('subheading').classList.add('transition')
+		})
+})
